9d6a1e98e0c90b67b9c47aca8c35554806ea1c90
Li-Wen Hsu <lwhsu@lwhsu.org>
2021-03-23 13:57:40 +0000

    Init

---
+++ porttest.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+PORTS="devel/valgrind devel/valgrind-devel"
+
+PORTSDIR=/usr/ports
+
+set -ex
+
+pwd
+
+cd /usr
+mv ports ports.old
+git clone --depth=1 --single-branch -b master https://github.com/freebsd/freebsd-ports.git ports
+
+cd ${CIRRUS_WORKING_DIR}
+for p in `cat list.txt`
+do
+	if [ -d ${PORTSDIR}/${p}/ ]; then
+		rm -fr ${PORTSDIR}/${p}/*
+	else
+		mkdir -p ${PORTSDIR}/${p}
+	fi
+        cp -R ./${p}/* ${PORTSDIR}/${p}/
+done
+
+mkdir /usr/ports/distfiles
+
+df -h
+
+echo "NO_ZFS=yes" >> /usr/local/etc/poudriere.conf
+echo "ALLOW_MAKE_JOBS=yes" >> /usr/local/etc/poudriere.conf
+sed -i.bak -e 's,FREEBSD_HOST=_PROTO_://_CHANGE_THIS_,FREEBSD_HOST=https://download.FreeBSD.org,' /usr/local/etc/poudriere.conf
+mkdir -p /usr/local/poudriere
+
+poudriere jail -c -j jail -v `uname -r`
+poudriere ports -c -f none -m null -M /usr/ports
+
+# bootstrap pkg repo
+poudriere bulk -t -j jail ports-mgmt/pkg
+
+for port in ${PORTS}; do
+	cd /usr/ports/${port}
+	make all-depends-list | awk -F'/' '{print $4"/"$5}' | xargs \
+	pkg fetch -y -o /usr/local/poudriere/data/packages/jail-default/.latest
+done
+
+set +e
+poudriere testport -j jail ${PORTS}
+RESULT=$?
+if [ ${RESULT} -eq 0 ]; then
+	exit 0
+fi
+set -e
+
+ls -l /usr/local/poudriere/data/logs/bulk/jail-default/latest/logs/errors
+for i in /usr/local/poudriere/data/logs/bulk/jail-default/latest/logs/errors/*.log
+do
+	echo ==== $i ====
+	cat $i
+done
+
+exit ${RESULT}
